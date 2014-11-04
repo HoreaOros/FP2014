00666daf8d909a44aec3bd9732a19dc5dbb77b73
Horea Oros <horea.oros@gmail.com>
2014-11-04 13:01:57 +0200

    Project Tablouri added

---
+++ Tablouri/Tablouri/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace Tablouri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] v;
+            v = new int[100];
+
+            Print(v);
+
+        }
+
+        static void Print(int[] v)
+        {
+            for(int i = 0; i < v.Length; i++)
+                Console.WriteLine(v[i]);
+        }
+    }
+}
